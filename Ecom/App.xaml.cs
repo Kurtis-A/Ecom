4d4dc77e98ee9fad242f8c0047f46836a7752633
Kurtis Aspden <kurtisga@gmail.com>
2021-11-23 23:31:35 +0000

    Implemented partial core Ui and Solution Framework

--- Ecom/App.xaml.cs
+++ Ecom/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Ecom.Services;
+using Ecom.Data.Repository;
+using Ecom.ViewModel.User;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using Ecom.Helpers;
+using ToastNotifications;
+using ToastNotifications.Position;
+using ToastNotifications.Lifetime;
+using System;
 
 namespace Ecom
 {
@@ -13,5 +16,55 @@ namespace Ecom
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            _serviceProvider = ConfigureServices().BuildServiceProvider();
+            ConfigureNotifications();
+        }
+
+        private void ConfigureNotifications()
+        {
+            Globals.Notifier = new Notifier(cfg =>
+            {
+                cfg.PositionProvider = new WindowPositionProvider(
+                    parentWindow: Current.MainWindow,
+                    corner: Corner.TopRight,
+                    offsetX: 10,
+                    offsetY: 55);
+
+                cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                    notificationLifetime: TimeSpan.FromSeconds(3),
+                    maximumNotificationCount: MaximumNotificationCount.FromCount(3));
+
+                cfg.Dispatcher = Current.Dispatcher;
+            });
+        }
+
+        private ServiceCollection ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            //Views
+            services.AddTransient<BaseWindow>();
+
+            //View Model
+            services.AddScoped<SingleUserViewModel>();
+
+            //Service
+            services.AddScoped<UserService>();
+
+            //Respository
+            services.AddScoped<UserRepository>();
+
+            return services;
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var window = _serviceProvider.GetService<BaseWindow>();
+            window.Show();
+        }
     }
 }
---
+++ Ecom/View/BaseWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Ecom.Helpers;
+using System.Windows;
+using ToastNotifications.Messages;
+
+namespace Ecom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class BaseWindow : Window
+    {
+        public BaseWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) => DragMove();
+
+        private void Close_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void Maximise_Click(object sender, RoutedEventArgs e)
+        {
+            Globals.Notifier.ShowInformation("Whoops! Not Implemented yet... Sorry :)");
+        }
+
+        private void Minimise_Click(object sender, RoutedEventArgs e)
+        {
+            Globals.Notifier.ShowInformation("Whoops! Not Implemented yet... Sorry :)");
+        }
+    }
+}
