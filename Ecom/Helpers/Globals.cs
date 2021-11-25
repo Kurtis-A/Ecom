ea64ba4e8145e6b35b1f3e723c71bab85ba38090
Kurtis Aspden <kurtisga@gmail.com>
2021-11-25 11:10:50 +0000

    Added Service Provider to Globals

--- Ecom/Helpers/Globals.cs
+++ Ecom/Helpers/Globals.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ToastNotifications;
 
 namespace Ecom.Helpers
 {
     public static class Globals
     {
+        public static IServiceProvider ServiceProvider { get; set; }
+
         public static Notifier Notifier { get; set; }
     }
 }
