40490908cfb5822670bf9f110ec3780bcf036fef
Kurtis Aspden <kurtisga@gmail.com>
2021-11-28 15:23:37 +0000

    Amended Base and Staff Repository

--- Ecom/Data/Repository/BaseRepository.cs
+++ Ecom/Data/Repository/BaseRepository.cs
@@ -7,39 +7,39 @@ namespace Ecom.Data.Repository
 {
     public abstract class BaseRepository<TEntity, TContext> where TEntity : class, IEntity where TContext : DbContext
     {
-        internal readonly TContext _context;
+        internal readonly TContext context;
 
         public BaseRepository(TContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
-        public virtual async Task<TEntity> Find(int id) => await _context.Set<TEntity>().FindAsync(id);
+        public virtual async Task<TEntity> Find(int id) => await context.Set<TEntity>().FindAsync(id);
 
-        public virtual async Task<List<TEntity>> FindAll() => await _context.Set<TEntity>().ToListAsync();
+        public virtual async Task<List<TEntity>> FindAll() => await context.Set<TEntity>().ToListAsync();
 
         public virtual async Task<TEntity> Add(TEntity entity)
         {
-            _context.Set<TEntity>().Add(entity);
-            await _context.SaveChangesAsync();
+            context.Set<TEntity>().Add(entity);
+            await context.SaveChangesAsync();
             return entity;
         }
 
         public virtual async Task<TEntity> Update(TEntity entity)
         {
-            _context.Entry(entity).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
             return entity;
         }
 
         public virtual async Task<TEntity> Delete(int id)
         {
-            var entity = await _context.Set<TEntity>().FindAsync(id);
+            var entity = await context.Set<TEntity>().FindAsync(id);
             if (entity == null)
                 return entity;
 
-            _context.Set<TEntity>().Remove(entity);
-            await _context.SaveChangesAsync();
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
 
             return entity;
         }
--- Ecom/Data/Repository/StaffRepository.cs
+++ Ecom/Data/Repository/StaffRepository.cs
@@ -1,12 +1,26 @@
-﻿namespace Ecom.Data.Repository
+﻿using Ecom.Data.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecom.Data.Repository
 {
-    public class StaffRepository
+    public class StaffRepository : BaseRepository<Staff, ApplicationDbContext>
     {
         private readonly ApplicationDbContext _context;
 
-        public StaffRepository()
+        public StaffRepository(ApplicationDbContext context) : base(context)
         {
+            _context = context;
+        }
 
+        public override Task<List<Staff>> FindAll()
+        {
+            return base.FindAll();
+        }
+
+        public override Task<Staff> Find(int id)
+        {
+            return base.Find(id);
         }
     }
 }
