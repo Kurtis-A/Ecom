9ccf33c5677216af305c584627ffbb135b5324ea
Kurtis Aspden <kurtisga@gmail.com>
2021-11-30 17:04:47 +0000

    Added Rota Repository

---
+++ Ecom/Data/Repository/RotaRepository.cs
@@ -0,0 +1,27 @@
+﻿using Ecom.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Data.Repository
+{
+    public class RotaRepository : BaseRepository<Rota, ApplicationDbContext>
+    {
+        public RotaRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public override Task<List<Rota>> FindAll()
+        {
+            return base.FindAll();
+        }
+
+        public override Task<Rota> Find(int id)
+        {
+            return base.Find(id);
+        }
+    }
+}
