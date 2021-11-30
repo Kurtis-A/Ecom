55dfc502e62084ae17d88f5d047d82ac9da46985
Kurtis Aspden <kurtisga@gmail.com>
2021-11-30 17:00:47 +0000

    Added Absence Repository

---
+++ Ecom/Data/Repository/AbsenceRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ecom.Data.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecom.Data.Repository
+{
+    public class AbsenceRepository : BaseRepository<Absence, ApplicationDbContext>
+    {
+        public AbsenceRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public override Task<List<Absence>> FindAll()
+        {
+            return base.FindAll();
+        }
+
+        public override Task<Absence> Find(int id)
+        {
+            return base.Find(id);
+        }
+    }
+}
