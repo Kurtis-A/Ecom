f1815fea8cc19fa97eafe72d76ef0c9ba057235c
Kurtis Aspden <kurtisga@gmail.com>
2021-12-02 21:48:15 +0000

    Added Update Method to Staff Service

--- Ecom/Services/StaffService.cs
+++ Ecom/Services/StaffService.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Ecom.Data.Model;
 using Ecom.Data.Repository;
 using Ecom.Helpers;
 using Ecom.ViewModel.Staff;
+using ToastNotifications.Messages;
 
 namespace Ecom.Services
 {
@@ -16,7 +18,7 @@ namespace Ecom.Services
             _staffRepository = staffRepository;
         }
 
-        public async Task<List<StaffListViewModel>> FetchAllUsers()
+        public async Task<List<StaffListViewModel>> FetchAllStaff()
         {
             var results = await _staffRepository.FindAll();
             return Globals.Mapper.Map<List<Staff>, List<StaffListViewModel>>(results);
@@ -28,6 +30,25 @@ namespace Ecom.Services
             var viewModel = new StaffViewModel();
 
             return results == null ? viewModel : Globals.Mapper.Map<StaffViewModel>(results);
+        }
+
+        public async Task<bool> UpdateStaffMember(StaffViewModel viewModel)
+        {
+            try
+            {
+                var entity = await _staffRepository.Find(viewModel.Id);
+                if (entity != null)
+                {
+                    entity = Globals.Mapper.Map(viewModel, entity);
+                    await _staffRepository.Update(entity);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Globals.Notifier.ShowError($"An error occured: {ex.Message}");
+            }
+            return false;
         }
     }
 }
