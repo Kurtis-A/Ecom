36ea9107ada136a76eb83d9bfab0f285affc7782
Kurtis Aspden <kurtisga@gmail.com>
2021-12-05 16:22:26 +0000

    Added Absence Feature for Read Only Viewing

---
+++ Ecom/Services/AbsenceService.cs
@@ -0,0 +1,24 @@
+﻿using Ecom.Data.Model;
+using Ecom.Data.Repository;
+using Ecom.Helpers;
+using Ecom.ViewModel.Absence;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ecom.Services
+{
+    public class AbsenceService
+    {
+        private readonly AbsenceRepository _absenceRepository;
+
+        public AbsenceService(AbsenceRepository absenceRepository)
+        {
+            _absenceRepository = absenceRepository;
+        }
+
+        public async Task<List<AbsenceViewModel>> FetchAllAbsences()
+        {
+            return Globals.Mapper.Map<List<Absence>, List<AbsenceViewModel>>(await _absenceRepository.FindAll());
+        }
+    }
+}
---
+++ Ecom/ViewModel/Absence/AbsenceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ecom.ViewModel.Absence
+{
+    public class AbsenceViewModel : ViewModelBase
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Type { get; set; }
+    }
+}
