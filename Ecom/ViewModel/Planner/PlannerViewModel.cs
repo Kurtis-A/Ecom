fbdc7d0e7a5a512f019ba0e7e71ddd7eff3532c8
Kurtis Aspden <kurtisga@gmail.com>
2021-12-11 19:34:22 +0000

    Ameneded Planner Vm with additional dates

--- Ecom/ViewModel/Planner/PlannerViewModel.cs
+++ Ecom/ViewModel/Planner/PlannerViewModel.cs
@@ -20,87 +20,124 @@ namespace Ecom.ViewModel.Planner
         private string day6Day;
         private string day7Day;
         private DateTime day1Date;
+        private DateTime day2Date;
+        private DateTime day3Date;
+        private DateTime day4Date;
+        private DateTime day5Date;
+        private DateTime day6Date;
+        private DateTime day7Date;
 
         public string Day1Day
         {
-            get => day1Day;
-            set
-            {
-                day1Day = value;
-                OnPropertyChanged();
-            }
+            get => day1Date.DayOfWeek.ToString();
         }
 
         public string Day2Day
         {
-            get => day2Day;
-            set
-            {
-                day2Day = value;
-                OnPropertyChanged();
-            }
+            get => day2Date.DayOfWeek.ToString();
         }
 
         public string Day3Day
         {
-            get => day3Day;
-            set
-            {
-                day3Day = value;
-                OnPropertyChanged();
-            }
+            get => day3Date.DayOfWeek.ToString();
         }
 
         public string Day4Day
         {
-            get => day4Day;
-            set
-            {
-                day4Day = value;
-                OnPropertyChanged();
-            }
+            get => day4Date.DayOfWeek.ToString();
         }
 
         public string Day5Day
         {
-            get => day5Day;
-            set
-            {
-                day5Day = value;
-                OnPropertyChanged();
-            }
+            get => day5Date.DayOfWeek.ToString();
         }
 
         public string Day6Day
         {
-            get => day6Day;
-            set
-            {
-                day6Day = value;
-                OnPropertyChanged();
-            }
+            get => day6Date.DayOfWeek.ToString();
         }
 
         public string Day7Day
         {
-            get => day7Day;
-            set
-            {
-                day7Day = value;
-                OnPropertyChanged();
-            }
+            get => day7Date.DayOfWeek.ToString();
         }
 
-        public DateTime Day1Date
+        public string Day1Date
         {
-            get => day1Date;
+            get => day1Date.ToString("dd MMM yy");
             set
             {
-                day1Date = value;
+                day1Date = DateTime.Parse(value);
                 OnPropertyChanged();
+                OnPropertyChanged(nameof(Day1Day));
             }
         }
 
+        public string Day2Date
+        {
+            get => day2Date.ToString("dd MMM yy");
+            set
+            {
+                day2Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day2Day));
+            }
+        }
+
+        public string Day3Date
+        {
+            get => day3Date.ToString("dd MMM yy");
+            set
+            {
+                day3Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day3Day));
+            }
+        }
+
+        public string Day4Date
+        {
+            get => day4Date.ToString("dd MMM yy");
+            set
+            {
+                day4Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day4Day));
+            }
+        }
+
+        public string Day5Date
+        {
+            get => day5Date.ToString("dd MMM yy");
+            set
+            {
+                day5Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day5Day));
+            }
+        }
+
+        public string Day6Date
+        {
+            get => day6Date.ToString("dd MMM yy");
+            set
+            {
+                day6Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day6Day));
+            }
+        }
+
+        public string Day7Date
+        {
+            get => day7Date.ToString("dd MMM yy");
+            set
+            {
+                day7Date = DateTime.Parse(value);
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Day7Day));
+            }
+        }
 
         public List<StaffViewModel> StaffAvailability
         {
