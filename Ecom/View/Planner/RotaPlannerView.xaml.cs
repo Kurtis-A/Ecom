6c697db43ac113bbb47f6b79f211545fe62a86df
Kurtis Aspden <kurtisga@gmail.com>
2021-12-11 19:33:54 +0000

    Refactored Date Generation

--- Ecom/View/Planner/RotaPlannerView.xaml.cs
+++ Ecom/View/Planner/RotaPlannerView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Ecom.Services;
+﻿using Ecom.Helpers;
+using Ecom.Services;
 using Ecom.ViewModel.Absence;
 using Ecom.ViewModel.Planner;
 using Ecom.ViewModel.Staff;
@@ -7,11 +8,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using ToastNotifications.Messages;
 
 namespace Ecom.View.Planner
 {
@@ -62,67 +65,34 @@ namespace Ecom.View.Planner
             RotaDate = DateTime.Today.AddMonths(1);
             SelectedDate.SelectedDate = RotaDate;
 
-            BuildRotaDates(RotaDate);
+            BuildViewDates(RotaDate);
 
             if (RotaType.SelectedItem != null)
                 Populate();
         }
 
-        public void BuildRotaDates(DateTime selectDate)
+        public void BuildViewDates(DateTime date)
         {
-            var rotaWeek = new Dictionary<string, DateTime>();
-
-            for (var i = 0; i < 7; i++)
+            try
             {
-                var date = selectDate.AddDays(i);
-                rotaWeek.Add(date.DayOfWeek.ToString(), date);
+                PropertyInfo[] properties = ViewModel.GetType().GetProperties();
+
+                int count = 0;
+                foreach (var property in properties.Where(x => x.Name.Contains("Date")).OrderBy(o => o.Name))
+                {
+                    //property.GetType().GetProperty(property.Name).SetValue(ViewModel, date.AddDays(count).ToString());
+
+                    var pType = property.GetType();
+                    var pProp = pType.GetProperty(property.Name);
+                    pProp.SetValue(ViewModel, date.AddDays(count).ToString());
+                    count++;
+                }
             }
-
-            var rotaDates = rotaWeek.ToList();
-
-            //Availability Population
-            Day1_Day.Text = rotaDates[0].Key;
-            Day1_Date.Text = rotaDates[0].Value.ToString("dd MMM yy");
-
-            Day2_Day.Text = rotaDates[1].Key;
-            Day2_Date.Text = rotaDates[1].Value.ToString("dd MMM yy");
-
-            Day3_Day.Text = rotaDates[2].Key;
-            Day3_Date.Text = rotaDates[2].Value.ToString("dd MMM yy");
-
-            Day4_Day.Text = rotaDates[3].Key;
-            Day4_Date.Text = rotaDates[3].Value.ToString("dd MMM yy");
-
-            Day5_Day.Text = rotaDates[4].Key;
-            Day5_Date.Text = rotaDates[4].Value.ToString("dd MMM yy");
-
-            Day6_Day.Text = rotaDates[5].Key;
-            Day6_Date.Text = rotaDates[5].Value.ToString("dd MMM yy");
-
-            Day7_Day.Text = rotaDates[6].Key;
-            Day7_Date.Text = rotaDates[6].Value.ToString("dd MMM yy");
-
-            //Rota Population
-            Day1_DayRota.Text = rotaDates[0].Key;
-            Day1_DateRota.Text = rotaDates[0].Value.ToString("dd MMM yy");
-
-            Day2_DayRota.Text = rotaDates[1].Key;
-            Day2_DateRota.Text = rotaDates[1].Value.ToString("dd MMM yy");
-
-            Day3_DayRota.Text = rotaDates[2].Key;
-            Day3_DateRota.Text = rotaDates[2].Value.ToString("dd MMM yy");
-
-            Day4_DayRota.Text = rotaDates[3].Key;
-            Day4_DateRota.Text = rotaDates[3].Value.ToString("dd MMM yy");
-
-            Day5_DayRota.Text = rotaDates[4].Key;
-            Day5_DateRota.Text = rotaDates[4].Value.ToString("dd MMM yy");
-
-            Day6_DayRota.Text = rotaDates[5].Key;
-            Day6_DateRota.Text = rotaDates[5].Value.ToString("dd MMM yy");
-
-            Day7_DayRota.Text = rotaDates[6].Key;
-            Day7_DateRota.Text = rotaDates[6].Value.ToString("dd MMM yy");
+            catch (Exception e)
+            {
+                Globals.Notifier.ShowError($"Opps something went wrong!");
+                Globals.Notifier.ShowInformation($"Error Info: {e.Message}");
+            }
         }
 
         private void Populate()
@@ -222,7 +192,7 @@ namespace Ecom.View.Planner
                     break;
             }
 
-            Chip chip = new Chip()
+            Chip chip = new()
             {
                 Content = username,
                 Icon = icon,
@@ -240,25 +210,37 @@ namespace Ecom.View.Planner
             var chipType = sender as Chip;
             var parent = chipType.Parent;
 
-            AddStaffToRota(chipType, parent);
+            ChipSwitch(chipType, parent);
         }
 
-        private bool AddStaffToRota(Chip StaffMember, DependencyObject Parent)
+        private bool ChipSwitch(Chip StaffMember, DependencyObject parent)
         {
             //Remove from Available Panel
-            var previousPanel = Parent as WrapPanel;
+            var previousPanel = (WrapPanel)parent;
             previousPanel.Children.Remove(StaffMember);
 
+
             //Reassign PanelName
-            var parentName = Parent.GetValue(NameProperty);
-            var newParentstring = parentName.ToString().Replace("Avail", "Rota");
+            var parentName = string.Empty;
+            if (parent.GetValue(NameProperty).ToString().Contains("Avail"))
+            {
+                //Add Staff to Rota
+                parentName = parent.GetValue(NameProperty).ToString().Replace("Avail", "Rota");
+            }
+                
 
-            //Find new Parent Object
-            Parent.SetValue(NameProperty, newParentstring);
-            var rotaPanel = (WrapPanel)LogicalTreeHelper.FindLogicalNode(GridRota,newParentstring);
+            if (parent.GetValue(NameProperty).ToString().Contains("Rota"))
+            {
+                //Remove Staff from Rota
+                parentName = parent.GetValue(NameProperty).ToString().Replace("Rota", "Avail");
+            }
+                
 
-            rotaPanel.Children.Add(StaffMember);
-            return false;   
+            //Find New Panel and add item to new panel
+            var newPanel = GridAvail.FindName(parentName) as WrapPanel;
+            newPanel.Children.Add(StaffMember);
+
+            return false;
         }
 
         protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
@@ -277,7 +259,7 @@ namespace Ecom.View.Planner
 
             if (date == RotaDate) return;
 
-            BuildRotaDates(date);
+            BuildViewDates(date);
             Populate();
         }
 
@@ -288,7 +270,7 @@ namespace Ecom.View.Planner
                 var selected = (DateTime)SelectedDate?.SelectedDate;
                 var last = selected.AddDays(-7);
                 SelectedDate.Text = last.ToShortDateString();
-                BuildRotaDates(last);
+                BuildViewDates(last);
             }
 
             Populate();
@@ -301,7 +283,7 @@ namespace Ecom.View.Planner
                 var selected = (DateTime)SelectedDate?.SelectedDate;
                 var next = selected.AddDays(7);
                 SelectedDate.Text = next.ToShortDateString();
-                BuildRotaDates(next);
+                BuildViewDates(next);
             }
 
             Populate();
